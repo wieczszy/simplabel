5d30e7a0ed3110c68fe03437bc5e68a447380109
fivtyen <5509131+fivtyen@users.noreply.github.com>
2019-09-04 22:46:55 +0200

    move GUI to a class

--- app.py
+++ app.py
@@ -4,29 +4,7 @@ import os
 import logging
 import tkinter as tk
 from utils.image_tracker import ImageTracker
-
-
-def callback():
-    if not e.get():
-        tk.messagebox.showerror("Error", "Enter your ID!")
-    elif not v.get():
-        tk.messagebox.showerror("Error", "Select one option!")
-    else:
-        print('{},{},{}'.format(e.get(), it.get_filename(), v.get()))
-        with open(ANSWERS_FILE, 'a') as f:
-            f.write('{},{},{}\n'.format(e.get(), it.get_filename(), v.get()))
-        try:
-            it.update_index()
-            new_img = it.get_image()
-            im_label.configure(image=new_img)
-            im_label.image = new_img
-        except IndexError:
-            logging.info("There is no more images to annotate")
-            tk.messagebox.showinfo("You're done!", "The is no more images")
-            exit()
-
-def close_window(): 
-    master.destroy()
+from utils.gui import SimplabelGUI
 
 if __name__ == "__main__":
 
@@ -35,29 +13,9 @@ if __name__ == "__main__":
 
     IMAGES_DIR = config['DIRS']['IMAGES_DIR']
     ANSWERS_FILE = config['DIRS']['ANSWERS_FILE']
-    CLASSES = config['ANNOTATION']['CLASSES'].split(',')
     IMAGE_SIZE = tuple([int(x) for x in config['IMAGES']['SIZE'].split(',')])
 
     master = tk.Tk()
-    master.title("Image annotation tool")
-
-    v = tk.StringVar()
-    e = tk.StringVar()
-
     it = ImageTracker(IMAGES_DIR, ANSWERS_FILE, IMAGE_SIZE)
-    img = it.get_image()
-
-    tk.Label(master, text="Annotator ID").pack(anchor=tk.W)
-    tk.Entry(textvariable=e).pack(anchor=tk.W)
-    tk.Label(master, text="Select the class that describes the image the best.").pack(anchor=tk.W)
-
-    im_label = tk.Label(master, image=img)
-    im_label.pack(anchor=tk.W)
-
-    for i in range(len(CLASSES)):
-        tk.Radiobutton(master, text=CLASSES[i], variable=v, value=CLASSES[i]).pack(anchor=tk.W)
-
-    tk.Button(master, text="Submit", command=callback).pack(anchor=tk.W)
-    tk.Button(master, text="Exit", command=close_window).pack(anchor=tk.W)
-
+    gui = SimplabelGUI(master, it, config)
     tk.mainloop()
---
+++ utils/gui.py
@@ -0,0 +1,61 @@
+import configparser
+import json
+import os
+import logging
+import tkinter as tk
+from tkinter import messagebox
+from utils.image_tracker import ImageTracker
+
+
+class SimplabelGUI():
+    def __init__(self, master, it, config):
+        self.master = master
+        self.config = config
+        self.it = it
+        self.v = tk.StringVar()
+        self.e = tk.StringVar()
+        self.classes = config['ANNOTATION']['CLASSES'].split(',')
+        self.answers_file = config['DIRS']['ANSWERS_FILE']
+
+        self.master.title("Image annotation tool")
+        self.img = self.it.get_image()
+
+        tk.Label(self.master, text="Annotator ID").pack(anchor=tk.W)
+        tk.Entry(textvariable=self.e).pack(anchor=tk.W)
+        tk.Label(self.master, text="Select the class that describes the image the best.").pack(anchor=tk.W)
+
+        self.im_label = tk.Label(self.master, image=self.img)
+        self.im_label.pack(anchor=tk.W)
+
+        for i in range(len(self.classes)):
+            tk.Radiobutton(self.master, 
+                           text=self.classes[i], 
+                           variable=self.v, 
+                           value=self.classes[i]).pack(anchor=tk.W)
+
+        tk.Button(self.master, text="Submit", command=self.callback).pack(anchor=tk.W)
+        tk.Button(self.master, text="Exit", command=self.close_window).pack(anchor=tk.W)
+
+    def callback(self):
+        if not self.e.get():
+            logging.info("Annotator ID has not been entered.")
+            messagebox.showerror("Error", "Enter your ID!")
+        elif not self.v.get():
+            logging.error("Class has not been selected.")
+            messagebox.showerror("Error", "Select class!")
+        else:
+            print('{},{},{}'.format(self.e.get(), self.it.get_filename(), self.v.get()))
+            with open(self.answers_file, 'a') as f:
+                f.write('{},{},{}\n'.format(self.e.get(), self.it.get_filename(), self.v.get()))
+            try:
+                self.it.update_index()
+                new_img = self.it.get_image()
+                self.im_label.configure(image=new_img)
+                self.im_label.image = new_img
+            except IndexError:
+                logging.error("There is no more images to annotate.")
+                messagebox.showinfo("You're done!", "The is no more images.")
+                exit()
+
+    def close_window(self): 
+        self.master.destroy()
